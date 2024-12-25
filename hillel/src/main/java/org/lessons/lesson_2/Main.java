7f649f8f22d5914161155aa1b751332cff965730
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2024-12-25 16:06:19 +0200

    km to miles

--- hillel/src/main/java/org/lessons/lesson_2/Main.java
+++ hillel/src/main/java/org/lessons/lesson_2/Main.java
@@ -6,15 +6,29 @@ public class Main {
     private static final double CONV_D = 1.609344;
 
     public static void main(String[] args) {
+        System.out.println("This program converts miles to kilometers and vice versa.");
+
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("This program converts miles to kilometers.");
-        System.out.println("Please, enter miles");
-
-        double miles = sc.nextDouble();
-        double km = miles * CONV_D;
-        System.out.println("Converted successfully - " + km + " km");
-
+        System.out.println("First of all: what do you want to convert?\n1.Miles --> Km\n2.Km --> Miles");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1: {
+                System.out.println("Please, enter miles");
+                double miles = sc.nextDouble();
+                double km = miles * CONV_D;
+                System.out.println("Converted successfully - " + km + " km");
+                break;
+            }
+            case 2: {
+                System.out.println("Please, enter kilometers");
+                double km = sc.nextDouble();
+                double miles = km / CONV_D;
+                System.out.println("Converted successfully - " + miles + " miles");
+                break;
+            }
+            default: {
+                System.out.println("Invalid choice. You`ve entered bullshit. Try again.");
+            }
+        }
     }
 }
-
