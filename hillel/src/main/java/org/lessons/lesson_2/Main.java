c63eaafc9569f40ef12a0ff190bbf5e95946bcb0
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2024-12-25 16:01:25 +0200

    miles to km

--- hillel/src/main/java/org/lessons/lesson_2/Main.java
+++ hillel/src/main/java/org/lessons/lesson_2/Main.java
@@ -3,7 +3,18 @@ package org.lessons.lesson_2;
 import java.util.Scanner;
 
 public class Main {
+    private static final double CONV_D = 1.609344;
+
     public static void main(String[] args) {
-        System.out.println("This program converts miles to kilometers and vice versa.");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This program converts miles to kilometers.");
+        System.out.println("Please, enter miles");
+
+        double miles = sc.nextDouble();
+        double km = miles * CONV_D;
+        System.out.println("Converted successfully - " + km + " km");
+
     }
 }
+
