6942ee4d04aa0038aa0aa9a39cd7d4ea1c49df41
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2024-12-28 16:35:43 +0200

    added the dumb Fahrenheit system

--- hillel/src/main/java/org/lessons/lesson_3/Main.java
+++ hillel/src/main/java/org/lessons/lesson_3/Main.java
@@ -1,7 +1,23 @@
 package org.lessons.lesson_3;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final double CONV_CELSIUS = 32; //constant for the formula
+
     public static void main(String[] args) {
         System.out.println("This program converts fahrenheit to celsius.");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter fahrenheit: ");
+
+        double fahrenheit = sc.nextDouble();
+        double celsius = convertFahrenheitToCelsius(fahrenheit);
+        System.out.println("The celsius is " + celsius);
+
+    }
+
+    private static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - CONV_CELSIUS) * 5 / 9; //
     }
 }
