158768f4d8419c8579acf2fcec03417bc93b33ca
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2024-12-28 16:17:59 +0200

    degree converter

---
+++ hillel/src/main/java/org/lessons/lesson_3/Main.java
@@ -0,0 +1,7 @@
+package org.lessons.lesson_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This program converts fahrenheit to celsius.");
+    }
+}
