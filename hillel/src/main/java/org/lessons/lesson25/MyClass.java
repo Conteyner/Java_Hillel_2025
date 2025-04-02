1cab5fdd74816b097d281b1e6e19c3f91b8a65ee
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2025-04-03 00:37:30 +0300

    Singleton

---
+++ hillel/src/main/java/org/lessons/lesson25/MyClass.java
@@ -0,0 +1,8 @@
+package org.lessons.lesson25;
+
+public class MyClass {
+    public static void doSomething() {
+        Logger log = Logger.getInstance();
+        log.log("Message from MyClass");
+    }
+}
