e95764cb7f130ab6c5b4bc086e5f0f290fba4314
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2025-04-24 04:05:09 +0300

    Update DatabaseConnector.java

--- hillel/src/main/java/org/lessons/lesson30/DatabaseConnector.java
+++ hillel/src/main/java/org/lessons/lesson30/DatabaseConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/company";
     private static final String USER = "root";
-    private static final String PASSWORD = "42255a";
+    private static final String PASSWORD = "root";
     private Connection connection;
 
     public void connect() throws SQLException {
