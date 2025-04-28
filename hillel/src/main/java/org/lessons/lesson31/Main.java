7bc1c34ff0f394c7f88741ac6d8f27dcf8335d06
Conteynerrrrr <122826855+Conteyner@users.noreply.github.com>
2025-04-28 05:27:38 +0300

    душно

---
+++ hillel/src/main/java/org/lessons/lesson31/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.lesson31;
+
+import org.lessons.lesson31.dao.StudentDaoImpl;
+import org.lessons.lesson31.dto.Homework;
+import org.lessons.lesson31.dto.Student;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
